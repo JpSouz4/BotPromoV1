2dfa06bc7e0d440e0227597459647994607b0c7d
JpSouz4 <joao02pedro@gmail.com>
2024-11-10 23:44:57 -0300

    add convert csv to json

--- services.js
+++ services.js
@@ -3,7 +3,12 @@ const emojilib = require("emojilib");
 
 const fs = require('fs');
 const csv = require('csvtojson');
+let csvToJson = require('convert-csv-to-json');
 const axios = require('axios');
+
+const datafeedCSV= 'BotPromoV1/DATA/datafeed.csv'
+const datafeed='BotPromoV1/DATA/datafeed.json'
+
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,6 +18,18 @@ function chatIdGrupo() {
   const chatId = '-4563696034'
   return chatId
 }
+
+function convertCSV(datafeedCSV, datafeed){
+  
+  try {
+    csvToJson.generateJsonFileFromCsv(datafeedCSV,datafeed);
+  } catch (ex) {
+    console.error('Erro ao salvar o JSON:', ex);
+  }
+  
+}
+
+convertCSV(datafeedCSV, datafeed);
 
 module.exports = {
   randomNumber,
