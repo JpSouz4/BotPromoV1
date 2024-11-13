67feda9fde8252f6f93e4523ec1806da138a42a3
JpSouz4 <joao02pedro@gmail.com>
2024-11-13 19:24:30 -0300

    fix chatid do bot do whats

--- whats.js
+++ whats.js
@@ -4,19 +4,21 @@ const qrcode = require('qrcode-terminal');
 const emoji = require("node-emoji");
 const emojilib = require("emojilib");
 
-const groupId = '5518991229015-1616757881@g.us'; 
-
 const {
-    convertCSV,
-    enviarItem,
     randomNumber,
-    chatIdGrupo, } = require('./services.js');
+    chatIdGrupo,
+    groupId,
+    convertCSV,
+    enviarItem
+} = require('./services.js');
 
-    const fire = emoji.find('🔥');
-    const blue_circle = emoji.find('🔵');
-    const rotating_light = emoji.find('🚨');
-    const mark = emoji.find('📌');
-    const dollar = emoji.find('💵');
+const WgroupId = groupId(); 
+
+const fire = emoji.find('🔥');
+const blue_circle = emoji.find('🔵');
+const rotating_light = emoji.find('🚨');
+const mark = emoji.find('📌');
+const dollar = emoji.find('💵');
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -34,14 +36,17 @@ const client = new Client({
     console.log(group.id._serialized);
 }); */
 
+client.on('qr', (qr) => { 
+    qrcode.generate(qr, { small: true }); 
+});
+
 client.on('ready', async () => {
 
     console.log('Bot on!');
-  
+
 });
 
 client.initialize();
-
 
 async function verificarEstado() {
 
@@ -49,36 +54,36 @@ async function verificarEstado() {
     console.log('Estado atual do bot:', state);
 
     if (state === 'CONNECTED') {
-        
+
         const message = await convertCSV()
-                  .then((result) => {
-        
-                      let valor = (result.Price);
-                      const valorNumerico = parseFloat(valor.replace(/[^\d.,]/g, '').replace(',', '.'));
-                      let valorFormatado = valorNumerico.toLocaleString('pt-BR', {
-                          style: 'currency',
-                          currency: 'BRL'
-                      });
-        
-                      const resposta = `${fire.emoji} ${result.ItemName} 
+            .then((result) => {
+
+                let valor = (result.Price);
+                const valorNumerico = parseFloat(valor.replace(/[^\d.,]/g, '').replace(',', '.'));
+                let valorFormatado = valorNumerico.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                });
+
+                const resposta = `${fire.emoji} ${result.ItemName} 
                       \n ${dollar.emoji} Valor: ${valorFormatado}
                       \n Acesse o link: ${result.OfferLink}
                       \n ${blue_circle.emoji} Redes Sociais: `;
-                    
-                      return resposta
-                    })
-                
-                    await client.sendMessage(groupId, message).then(response => {
-        
-                console.log('Mensagem enviada com sucesso:', message);
-        
-            }).catch(err => {
-                console.error('Erro ao enviar mensagem:', err);
-            });
+
+                return resposta
+            })
+
+        await client.sendMessage(WgroupId, message).then(response => {
+
+            console.log('Mensagem enviada com sucesso:', message);
+
+        }).catch(err => {
+            console.error('Erro ao enviar mensagem:', err);
+        });
 
     } else if (state === 'DISCONNECTED') {
         console.log('Bot está desconectado.');
     }
 };
-        
+
 setInterval(verificarEstado, 60000);
